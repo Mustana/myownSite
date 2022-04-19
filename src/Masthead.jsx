36b954c30557f7bc55df50d903c9461449b5fa3c
Mustafa <mustu02@gmail.com>
2022-04-19 12:09:59 +0400

    Removed Hello World

--- src/Masthead.jsx
+++ src/Masthead.jsx
@@ -12,7 +12,6 @@ const Masthead = () => {
      <header className="masthead" id="Masthead">
         <div className="container">
             <div className="masthead-heading">Mustafa Kushalgadhwala</div>
-            <h2>Hello World</h2>
             <div className="masthead-subheading">Creative Digital Developer</div>
         <div className="masthead-subheadingmore">Creative developer, animator and team leader with 15 years of experience, creating websites, games, web components, animations, social media content, rich media interactive banner ads, HTML5 banners and more. </div>
         <div><ImMobile2 color="white" fontSize="1.5em"/> : +971 50 352 9579 | <ImMail color="white" fontSize="1.5em"/> : mustu02@gmail.com</div>
